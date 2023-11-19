4ef15a81edb0ca651ebf8727b55adf9fca747dd5
Rays Project <raynar0612abiyudiera@gmail.com>
2023-11-19 15:58:47 +0700

    19/11/23

--- script/script.js
+++ script/script.js
@@ -8,7 +8,7 @@ document.getElementById("unlock-btn").addEventListener("click", checkPassword);
 function checkPassword() {
     const enteredPassword = passwordInput.value;
     // You can change the password as needed
-    const correctPassword = "PramtiWSC";
+    const correctPassword = "RaysDev2123";
 
     if (enteredPassword === correctPassword) {
         lockScreen.style.display = "none";
@@ -22,7 +22,7 @@ function checkPassword() {
 document.getElementById("mulai").addEventListener("click", function () {
     const enteredPassword = passwordInput.value;
     // You can change the password as needed
-    const correctPassword = "PramtiWSC";
+    const correctPassword = "RaysDev2123";
 
     if (enteredPassword === correctPassword) {
         lockScreen.style.display = "none";
